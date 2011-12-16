61d5837af4b3eed333d758f68e787e616a57dfb7
Zabbal <suarev@stud.ntnu.no>
2011-12-16 23:29:39 +0800

    add sql-injection protection

--- setlog.pl
+++ setlog.pl
@@ -5,10 +5,13 @@ use warnings;
 use DBI;                                                                                                                  
 
 # prerequisites:
-# sudo aptitude install libdbd-sqlite3-perl                                                                               
+# sudo aptitude install libdbd-sqlite3-perl
 
 # hardcoded filename from sipauthserve.cpp
-my $db = DBI->connect("dbi:SQLite:dbname=/etc/OpenBTS/sipauthserve.db","","") or die;                                            
+my $db = DBI->connect("dbi:SQLite:dbname=/etc/OpenBTS/sipauthserve.db","","") or die;
+
+# This script rely on placeholders to prevent SQL-injection attacks
+# N. B. above-mentioned statement tells more about my (mis)understanding of SQLi rather than about actual script security
 
 # be verbose?
 my $verbose = 1;
@@ -16,18 +19,20 @@ my $verbose = 1;
 if(@ARGV != 2) { print "Usage example: $0 FileName.cpp DEBUG\n"; exit 1; }
 
 my $key = 'Log.Level.'.$ARGV[0];
-my $exist = $db->selectrow_array(qq/select VALUESTRING from CONFIG where "KEYSTRING" == "$key"/);                         
+my $exist = $db->selectrow_array(qq/select VALUESTRING from CONFIG where "KEYSTRING" == ?/, undef, "$key");
 
 if($exist)
 {
     print "$key found: $exist" if $verbose;
-    $db->do("UPDATE CONFIG SET VALUESTRING=\"".$ARGV[1]."\" WHERE KEYSTRING==\"".$key."\"");
+    my $plh = $db->prepare("UPDATE CONFIG SET VALUESTRING=? WHERE KEYSTRING==?");
+    $plh->execute($ARGV[1], "$key");
 }
 else
 {
     print "$key NOT found" if $verbose;
-    $db->do("INSERT INTO CONFIG (KEYSTRING,VALUESTRING,OPTIONAL) VALUES (\"".$key."\",\"".$ARGV[1]."\",1)");
+    my $plh = $db->prepare("INSERT INTO CONFIG (KEYSTRING,VALUESTRING,OPTIONAL) VALUES (?,?,1)");
+    $plh->execute($key, $ARGV[1]);
 }
 
-print ", set to ".$db->selectrow_array(qq/select VALUESTRING from CONFIG where "KEYSTRING" == "$key"/)."\n" if $verbose;
+print ", set to ".$db->selectrow_array(qq/select VALUESTRING from CONFIG where "KEYSTRING" == ?/, undef, "$key")."\n" if $verbose;
 $db->disconnect;
