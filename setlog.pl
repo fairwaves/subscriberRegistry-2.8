50c2a4b23a094d4425365ffe3680d305c2a5c3d8
Zabbal <suarev@stud.ntnu.no>
2011-12-16 23:39:57 +0800

    add view-only functionality

--- setlog.pl
+++ setlog.pl
@@ -16,7 +16,7 @@ my $db = DBI->connect("dbi:SQLite:dbname=/etc/OpenBTS/sipauthserve.db","","") or
 # be verbose?
 my $verbose = 1;
 
-if(@ARGV != 2) { print "Usage example: $0 FileName.cpp DEBUG\n"; exit 1; }
+if(@ARGV != 2 or @ARGV != 1) { die "Usage example: $0 FileName.cpp [DEBUG]\n"; }
 
 my $key = 'Log.Level.'.$ARGV[0];
 my $exist = $db->selectrow_array(qq/select VALUESTRING from CONFIG where "KEYSTRING" == ?/, undef, "$key");
@@ -25,14 +25,17 @@ if($exist)
 {
     print "$key found: $exist" if $verbose;
     my $plh = $db->prepare("UPDATE CONFIG SET VALUESTRING=? WHERE KEYSTRING==?");
-    $plh->execute($ARGV[1], "$key");
+    $plh->execute($ARGV[1], "$key") if $ARGV[1];
 }
 else
 {
     print "$key NOT found" if $verbose;
     my $plh = $db->prepare("INSERT INTO CONFIG (KEYSTRING,VALUESTRING,OPTIONAL) VALUES (?,?,1)");
-    $plh->execute($key, $ARGV[1]);
+    $plh->execute($key, $ARGV[1]) if $ARGV[1];
 }
 
-print ", set to ".$db->selectrow_array(qq/select VALUESTRING from CONFIG where "KEYSTRING" == ?/, undef, "$key")."\n" if $verbose;
+if($ARGV[1] and $verbose)
+{
+    print ", set to ".$db->selectrow_array(qq/select VALUESTRING from CONFIG where "KEYSTRING" == ?/, undef, "$key")."\n";
+}
 $db->disconnect;
