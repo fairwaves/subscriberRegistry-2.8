78ec8849f81f37e30a650cc6f0dbea25e76e07f7
Zabbal <suarev@stud.ntnu.no>
2012-01-21 01:13:00 +0800

    transmit Kc in sip 200 OK response

--- sipauthserve.cpp
+++ sipauthserve.cpp
@@ -218,19 +218,22 @@ char *processBuffer(char *buffer)
 		} else {
 			string kc;
 			bool sres_good = authenticate(imsi, RAND, SRES, &kc);
-			LOG(INFO) << "imsi known, 2nd register, good = " << sres_good;
-			if (sres_good) {
-		// sres matches rand => 200 OK
-		osip_message_set_status_code (response, 200);
-		osip_message_set_reason_phrase (response, osip_strdup("OK"));
-		// And register it.
-		LOG(INFO) << imsi << " success, registering for IP address " << remote_host << ":" << remote_port;
-		imsiSet(imsi,"ipaddr",remote_host);
-		imsiSet(imsi,"port",remote_port);
-			} else {
-				// sres does not match rand => 401 Unauthorized
-				osip_message_set_status_code (response, 401);
-				osip_message_set_reason_phrase (response, osip_strdup("Unauthorized"));
+			LOG(INFO) << "imsi known, 2nd register, auth = " << sres_good;
+			if (sres_good) {// sres matches rand => 200 OK
+			  osip_message_set_status_code (response, 200);
+			  osip_message_set_reason_phrase (response, osip_strdup("OK"));
+			  osip_authentication_info_t * auth_header;
+			  osip_authentication_info_init (&auth_header);
+			  osip_authentication_info_set_rspauth (auth_header, osip_strdup(kc.c_str()));
+			  osip_authentication_info_set_qop_options (auth_header, "Kc");
+			  osip_message_set_authentication_info (response, "Kc");
+			  // And register it.
+			  LOG(INFO) << imsi << " success, registering for IP address " << remote_host;
+			  imsiSet(imsi, "ipaddr", remote_host);
+			  imsiSet(imsi, "port", remote_port);
+			} else {// sres does not match rand => 401 Unauthorized
+			  osip_message_set_status_code (response, 401);
+			  osip_message_set_reason_phrase (response, osip_strdup("Unauthorized"));
 			}
 		}
 	}
